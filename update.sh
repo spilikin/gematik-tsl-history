5a763cd161573995e199bac525ae38e9940f4c7f
Sergej Suskov <108782+spilikin@users.noreply.github.com>
2025-07-22 10:10:30 +0200

    Updated https://download.tsl.ti-dienste.de/ECC/ECC-RSA_TSL.xml

--- update.sh
+++ update.sh
@@ -60,6 +60,16 @@ for URL in "${URLS[@]}"; do
     else
         echo "Error: HTTP status $HTTP_STATUS for URL: $URL" >&2
     fi
+
+    # If the file is an XML file, generate a pretty-printed version
+    if [[ "$FILENAME" == *.xml || "$FILENAME" == *.XML ]]; then
+        PRETTY_FILE="$TARGET_DIR/${FILENAME%.xml}-pretty.xml"
+        python -c 'import sys;import xml.dom.minidom;s=sys.stdin.read();print(xml.dom.minidom.parseString(s).toprettyxml())' < "$TARGET_DIR/$FILENAME" > "$PRETTY_FILE"
+        echo "Generated pretty-printed version: '$PRETTY_FILE'"
+        # Add pretty-printed file to git and commit
+        git add "$PRETTY_FILE"
+        git commit -m "Added pretty-printed version of $URL"
+    fi
 done
 
 # push the changes to the remote repository
